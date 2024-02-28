ccd9c0565b510f8312d8173d4d7766e7b7c0a20c
Onur Ucar <hello@ucaronur.com>
2024-02-28 00:09:39 -0500

    Fixed broken colorHelpers, removed unnecessary interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Palette from "./features/Palette";
 import { palettes } from "./app/common/seedColors";
-// import "./App.scss"
+import { generatePalette } from "./app/common/colorHelpers";
 
 
 function App() {
+  console.log(generatePalette(palettes[2] as any));
+
   return (
     <>
       <Palette {...palettes[2]} />
--- src/app/common/colorHelpers.ts
+++ src/app/common/colorHelpers.ts
@@ -1,46 +1,26 @@
 import chroma from "chroma-js";
 
-interface ColorInfo {
-  name: string;
-  id: string;
-  hex: string;
-  rgb: string;
-  rgba: string;
-}
-
-interface PaletteColor {
-  [level: number]: ColorInfo[];
-}
-
-interface Palette {
-  paletteName: string;
-  id: string;
-  emoji: string;
-  colors: PaletteColor;
-}
-
 const levels: number[] = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
-const generatePalette = (starterPalette: Palette): Palette => {
-  let newPalette: Palette = {
+function generatePalette(starterPalette: any): any {
+  let newPalette: any = {
     paletteName: starterPalette.paletteName,
     id: starterPalette.id,
     emoji: starterPalette.emoji,
     colors: {},
   };
-
+  
   for (let level of levels) {
     newPalette.colors[level] = [];
   }
 
-  // Use Object.entries to iterate over numeric keys and values
-  for (let [level, colorInfos] of Object.entries(starterPalette.colors)) {
-    let scale = getScale(colorInfos[0].hex, 10).reverse();
-
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
+    
     for (let i in scale) {
-      newPalette.colors[parseInt(level)].push({
-        name: `${colorInfos[0].name} ${levels[i]}`,
-        id: colorInfos[0].name.toLowerCase().replace(/ /g, "-"),
+      newPalette.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, "-"),
         hex: scale[i],
         rgb: chroma(scale[i]).css(),
         rgba: chroma(scale[i])
@@ -52,24 +32,24 @@ const generatePalette = (starterPalette: Palette): Palette => {
   }
 
   return newPalette;
-};
+}
 
-const getRange = (hexColor: string): string[] => {
+function getRange(hexColor: string): string[] {
   const end = "#fff";
   return [
     chroma(hexColor)
       .darken(1.4)
       .hex(),
     hexColor,
-    end,
+    end
   ];
-};
+}
 
-const getScale = (hexColor: string, numberOfColors: number): string[] => {
+function getScale(hexColor: string, numberOfColors: number): string[] {
   return chroma
     .scale(getRange(hexColor))
     .mode("lab")
     .colors(numberOfColors);
-};
+}
 
 export { generatePalette };
