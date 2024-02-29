1abcafd57fd88d5c29b5bc62f809787e865020dd
Onur Ucar <hello@ucaronur.com>
2024-02-29 13:40:56 -0500

    Added actual react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import Palette from "./features/Palette";
 import { palettes } from "./app/common/seedColors";
 import { generatePalette } from "./app/common/colorHelpers";
+import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
 
+const findPalette = id => palettes.find(palette => palette.id === id);
+
+const PaletteWrapper = () => {
+  const { id } = useParams();
+  const palette = generatePalette(findPalette(id))
+  return <Palette palette={palette} />;
+};
 
 function App() {
   console.log(generatePalette(palettes[4]));
 
+
   return (
-    <>
-        <Palette palette={generatePalette(palettes[4])} />
-    </>
+      <Routes>
+        <Route path="/" Component={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route path="/palette/:id" element={<PaletteWrapper />} />
+      </Routes>
   );
 }
 
