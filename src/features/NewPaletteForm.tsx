c023d31ef87a8b48f1c05b6829bd8ff1a38a027a
Onur Ucar <hello@ucaronur.com>
2024-03-01 03:10:57 -0500

    Added color chrome picker

--- src/features/NewPaletteForm.tsx
+++ src/features/NewPaletteForm.tsx
@@ -1,13 +1,76 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { ChromePicker } from "react-color";
+import { Typography } from "@mui/material";
+import "./styles/NewPaletteForm.scss";
 
-class NewPaletteForm extends Component {
-  render() {
-    return (
-      <div>
-        <h1>New Palette Form!</h1>
-      </div>
-    );
-  }
-}
+const drawerWidth = 400;
 
-export default NewPaletteForm;
+const CollapsibleDrawer = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <IconButton
+        color="inherit"
+        aria-label="Open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        className="drawer"
+        variant="persistent"
+        anchor="left"
+        open={open}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+      >
+        <div>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Typography variant="h4">Design Your Palette</Typography>
+        <div>
+          <Button variant="contained" color="secondary">
+            Clear Palette
+          </Button>
+          <Button variant="contained" color="primary">
+            Random Color
+          </Button>
+        </div>
+        <ChromePicker
+          color="pink"
+          onChangeComplete={(newColor) => console.log(newColor)}
+        />
+        <Button variant="contained" color="primary">
+          Add Color
+        </Button>
+      </Drawer>
+    </div>
+  );
+};
+
+export default CollapsibleDrawer;
