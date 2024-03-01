ae2ff0b88df258c227f43c02554ba9a0ff061c8a
Onur Ucar <hello@ucaronur.com>
2024-03-01 01:24:56 -0500

    Added conditional text-color rendering

--- src/features/ColorBox.tsx
+++ src/features/ColorBox.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./styles/ColorBox.scss";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 
 class ColorBox extends Component<any, any> {
   constructor(props) {
@@ -18,6 +19,10 @@ class ColorBox extends Component<any, any> {
   render() {
     const { name, background, showLink, paletteId, id } = this.props;
     const { copied } = this.state;
+
+    const isDarkColor = chroma(background).luminance() <= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.7;
+
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="colorBox">
@@ -27,19 +32,28 @@ class ColorBox extends Component<any, any> {
           />
           <div className={`copyMsg ${copied && "show"}`}>
             <h1>copied!</h1>
-            <p>{this.props.background}</p>
+            <p className={isLightColor ? "darkText" : ""}>
+              {this.props.background}
+            </p>
           </div>
           <div className="copy-container">
             <div className="boxContent">
-              <span>{name}</span>
+              <span className={isDarkColor ? "lightText" : ""}>{name}</span>
             </div>
-            <button className="copyButton">Copy</button>
+            <button className={`copyButton ${isLightColor && "darkText"}`}>
+              Copy
+            </button>
           </div>
 
           {/* Individual color palettes */}
           {showLink && (
-            <Link to={`/palette/${paletteId}/${id}`} onClick={(e) => e.stopPropagation()}>
-              <span className="seeMore">More</span>
+            <Link
+              to={`/palette/${paletteId}/${id}`}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <span className={`seeMore ${isLightColor && "darkText"}`}>
+                MORE
+              </span>
             </Link>
           )}
         </div>
