2e1ab00212929292abb3aa0e64cede70e801ac64
Onur Ucar <hello@ucaronur.com>
2024-03-04 22:36:18 -0500

    Created not found page with a gradient background

--- src/features/NotFound.tsx
+++ src/features/NotFound.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 import "./styles/NotFound.scss";
-import { BackgroundGradient } from "./BackgroundGradient";
+import { NotFoundGradientBackground } from "./NotFoundGradientBackground";
+import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => {
   return (
     <div className="notFound">
- <div>
-      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Air Jordan 4 Retro Reimagined
-        </p>
- 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-          February 17, 2024. Your best opportunity to get these right now is by
-          entering raffles and waiting for the official releases.
-        </p>
-        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-          <span>Buy now </span>
-          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-            $100
-          </span>
-        </button>
-      </BackgroundGradient>
-    </div>
+      <NotFoundGradientBackground>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="text-white text-center font-bold relative">
+            <p className="bg-clip-text mb-72 text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 text-3xl md:text-4xl lg:text-7xl">
+              Uh... 404! This page does not exist.
+            </p>
+            <div className="absolute inset-0 mt-72 z-50">
+              <Link
+                to="/"
+                className="text-3xl md:text-4xl lg:text-7xl cursor-pointer "
+              >
+                Click here to go back
+              </Link>
+            </div>
+          </div>
+        </div>
+      </NotFoundGradientBackground>
     </div>
   );
 };
